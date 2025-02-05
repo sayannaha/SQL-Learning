f0aef7a905fde637ab62f4ef698faa48b6c8d0a2
Sayan Naha <82594248+sayannaha@users.noreply.github.com>
2025-02-05 19:06:55 +0530

    Insert Data into the Table

---
+++ INSERT_INTO_TABLE.sql
@@ -0,0 +1,4 @@
+INSERT INTO Employee_Table (EMP_ID, FIRST_NAME, LAST_NAME, SALARY, DEPARTMENT, DEPT_ID, RATING)
+VALUES
+    (1, 'John', 'Doe', 50000.00, 'Engineering', 101, 4.5),
+    (2, 'Jane', 'Smith', 60000.00, 'Marketing', 102, 4.7);

d3da75bc7b43e0d689ce9a0e82025c5c45e52281
Sayan Naha <82594248+sayannaha@users.noreply.github.com>
2025-02-08 09:50:04 +0530

    HackerRank Advanced Problem Solutions

---
+++ HR_PROBLEM_10.sql
@@ -0,0 +1,14 @@
+--Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.--
+(
+    SELECT CITY, LENGTH(CITY) AS NameLength
+    FROM STATION
+    ORDER BY NameLength ASC, CITY ASC
+    LIMIT 1
+)
+UNION ALL
+(
+    SELECT CITY, LENGTH(CITY) AS NameLength
+    FROM STATION
+    ORDER BY NameLength DESC, CITY ASC
+    LIMIT 1
+);
